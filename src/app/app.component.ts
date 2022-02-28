a4e369ca1d5fa784999d9720dbae02d94347a1ea
shesha4kr <shesha4kr@gmail.com>
2022-02-28 07:47:17 +0530

    Added HomeComponent, NavBar and Routing Paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx-prac';
+  constructor(private router: Router) { }
+
+  home() {
+    this.router.navigate(['/']);
+    document.getElementById('home')?.classList.add('selected');
+    document.getElementById('counter')?.classList.remove('selected');
+    document.getElementById('posts')?.classList.remove('selected');
+  }
+
+  counter() {
+    this.router.navigate(['/counter']);
+    document.getElementById('home')?.classList.remove('selected');
+    document.getElementById('counter')?.classList.add('selected');
+    document.getElementById('posts')?.classList.remove('selected');
+  }
+
+  posts() {
+    document.getElementById('home')?.classList.remove('selected');
+    document.getElementById('counter')?.classList.remove('selected');
+    document.getElementById('posts')?.classList.add('selected');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
